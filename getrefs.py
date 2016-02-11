22696e9ce7abb6a438d3fcefd67015f673d439d3
Marcos Henrich <marcos.henrich@xamarin.com>
2016-02-11 17:28:51 +0000

    Added to getrefs.py support for ignoring refs.

--- getrefs.py
+++ getrefs.py
@@ -1,18 +1,21 @@
 import json
 import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
   sys.exit("Not enough args")
 json_file = str(sys.argv[1])
+ignore_refs = sys.argv[2:]
 
 json_data = open(json_file)
 data = json.load(json_data)
 refs = ""
 for key in data['dependencies'].keys():
-    refs += "-r:" + key + " "
+    if not key in ignore_refs:
+      refs += "-r:" + key + " "
 
-if data['frameworks'] and data['frameworks']['dnxcore50'] and data['frameworks']['dnxcore50']['dependencies']:
+if 'frameworks' in data and 'dnxcore50' in data['frameworks'] and 'dependencies' in data['frameworks']['dnxcore50']:
     for key in data['frameworks']['dnxcore50']['dependencies'].keys():
-        refs += "-r:" + key + " "
+        if not key in ignore_refs:
+          refs += "-r:" + key + " "
 
 print refs
